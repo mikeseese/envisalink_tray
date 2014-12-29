89e3e2add0812a0ed3cbf94578fcb4b555662917
Mike Seese <seesemichaelj@gmail.com>
2014-12-29 15:19:31 -0500

    Make connection and write out data read in

--- src/main.cpp
+++ src/main.cpp
@@ -6,12 +6,40 @@
 using namespace std;
 using boost::asio::ip::tcp;
 
-#define IP_ADDRESS "10.0.0.1"
+#define IP_ADDRESS "10.0.0.193"
+#define PORT "4025"
+#define PASSWORD "EatHealthy1"
 
 int main(int argc, char* argv[]) {
 	boost::asio::io_service io_service;
 	tcp::resolver resolver(io_service);
-	tcp::resolver::query query(IP_ADDRESS, "daytime?");
+	tcp::resolver::query query(IP_ADDRESS, PORT);
+	tcp::resolver::iterator endpoint_iterator = resolver.resolve(query);
+	tcp::resolver::iterator end;
+	tcp::socket socket(io_service);
+	boost::system::error_code error = boost::asio::error::host_not_found;
+
+	while(error && endpoint_iterator != end) {
+		socket.close();
+		socket.connect(*endpoint_iterator++, error);
+	}
+	if(error) {
+		throw boost::system::system_error(error);
+	}
+
+	for (;;)
+	{
+		boost::array<char, 128> buf;
+		boost::system::error_code error;
+
+		size_t len = socket.read_some(boost::asio::buffer(buf), error);
+		if (error == boost::asio::error::eof)
+			break; // Connection closed cleanly by peer.
+		else if (error)
+			throw boost::system::system_error(error); // Some other error.
+
+		std::cout.write(buf.data(), len);
+	}
 
 	return 0;
 }
