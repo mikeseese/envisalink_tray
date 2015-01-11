793712238afb4829a6bfabbd2c5282dff053189d
Mike Seese <seesemichaelj@gmail.com>
2015-01-11 14:28:44 -0500

    Added WIN make commands

--- make.bat
+++ make.bat
@@ -1,1 +1,3 @@
-cl -D_WIN32_WINNT=0x0501 /MD /EHsc /I\usr\include src\server.cpp /link /LIBPATH:\usr\lib 
+cl -D_WIN32_WINNT=0x0501 /MD /EHsc /I\usr\include src\main.cpp /link /LIBPATH:\usr\lib /Fbin/main.exe
+cl -D_WIN32_WINNT=0x0501 /MD /EHsc /I\usr\include src\server.cpp /link /LIBPATH:\usr\lib /Fbin/server.exe
+cl -D_WIN32_WINNT=0x0501 /MD /EHsc /I\usr\include src\client.cpp /link /LIBPATH:\usr\lib /Fbin/client.exe
