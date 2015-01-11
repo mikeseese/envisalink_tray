dbd5c0fbb79b9058007ecf3db4a3143a7158620e
Mike Seese <seesemichaelj@gmail.com>
2015-01-11 14:29:04 -0500

    Added temporary unix support

--- src/server.cpp
+++ src/server.cpp
@@ -25,6 +25,69 @@ struct elv3_tpi_command {
 	tcp::socket* client;
 };
 
+#ifdef __gnu_linux__
+
+void Sleep(unsigned int milliseconds) {
+	usleep(milliseconds*1000);
+}
+
+/* A utility function to reverse a string  */
+void reverse(char str[], int length)
+{
+    int start = 0;
+    int end = length -1;
+    while (start < end)
+    {
+        swap(*(str+start), *(str+end));
+        start++;
+        end--;
+    }
+}
+
+// Implementation of itoa()
+char* itoa(int num, char* str, int base)
+{
+    int i = 0;
+    bool isNegative = false;
+ 
+    /* Handle 0 explicitely, otherwise empty string is printed for 0 */
+    if (num == 0)
+    {
+        str[i++] = '0';
+        str[i] = '\0';
+        return str;
+    }
+ 
+    // In standard itoa(), negative numbers are handled only with 
+    // base 10. Otherwise numbers are considered unsigned.
+    if (num < 0 && base == 10)
+    {
+        isNegative = true;
+        num = -num;
+    }
+ 
+    // Process individual digits
+    while (num != 0)
+    {
+        int rem = num % base;
+        str[i++] = (rem > 9)? (rem-10) + 'a' : rem + '0';
+        num = num/base;
+    }
+ 
+    // If number is negative, append '-'
+    if (isNegative)
+        str[i++] = '-';
+ 
+    str[i] = '\0'; // Append string terminator
+ 
+    // Reverse the string
+    reverse(str, i);
+ 
+    return str;
+}
+
+#endif
+
 tcp::socket* elv3_tpi_socket;
 boost::asio::io_service elv3_tpi_io_service;
 vector<tcp::socket*> all_clients;
